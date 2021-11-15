be0b02e09e3d090b10762b6f19649568ce74ba9a
Alzhanov Tolegen <turkish_t@mail.ru>
2021-11-15 19:55:49 +0600

    Added next activity study

---
+++ app/src/main/java/com/lol/studyapp/activities/NextActivity.java
@@ -0,0 +1,17 @@
+package com.lol.studyapp.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.lol.studyapp.R;
+
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
---
+++ app/src/main/java/com/lol/studyapp/activities/ThirdActivity.java
@@ -0,0 +1,14 @@
+package com.lol.studyapp.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
