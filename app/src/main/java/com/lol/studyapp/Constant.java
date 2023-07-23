ca2537b3e1c952696b739e51ce44f1bdde763f2e
Tulya <33752668+tulya54@users.noreply.github.com>
2023-07-24 00:58:09 +0600

    Update Constant.java
    sdfdsfdsf

--- app/src/main/java/com/lol/studyapp/Constant.java
+++ app/src/main/java/com/lol/studyapp/Constant.java
@@ -2,5 +2,6 @@ package com.lol.studyapp;
 
 public class Constant {
     public static final String BASE_URL = "https://virtserver.swaggerhub.com/turkish_t/SUKA/1.0.0/";
+    public static final int BASE_LINK = 1234;
     // fdfdsfdsfdsfd
 }
